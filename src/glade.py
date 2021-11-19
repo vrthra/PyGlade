d4c666345285dd60c6e04d73a4958a904cbffcb2
Michael Mera <michael.mera@cispa.de>
2021-11-19 19:15:35 +0100

    improve description of phase 1
    Mostly removed it really. It added more confusion to the code by using a
    different vocabulary rather than the one from the paper. And I don't
    think it was accurate anyway.

--- src/glade.py
+++ src/glade.py
@@ -517,25 +517,17 @@ def get_dict(regex):
 def phase_1(alpha_in):
     # Active learning of regular right-hand side from Bastani et al.
     #
-    # The idea is as follows: We choose a single non-terminal to refine, and a
-    # single alternative at a time. Then, consider that single alternative as a
-    # string, with each token a character. Then apply regular expression
-    # synthesis to determine the abstraction candidates. Place each abstraction
-    # candidate as the replacement for that non-terminal, and generate the
-    # minimum string. Evaluate and verify that the string is accepted (adv:
-    # verify that the derivation tree is as expected). Do this for each
-    # alternative, and we have the list of actual alternatives.
-    #
-    # Seed input alpha_in is annotated rep(alpha_in).
-    # Then, each generalization step selects a single bracketed substring
+    # Each generalization step selects a single bracketed substring
     # T[alpha] and generates candidates based on decompositions of alpha
     # i.e. an expression of alpha as alpha = a_1, a_2, ..a_k
     #
     # Each iteration of the while loop corresponds to one generalization step.
     # The code below follows Algorithm 1, page 3 in the paper.
 
-    done = False
+    # Seed input alpha_in is annotated rep(alpha_in)
     curr_reg = One([alpha_in], 1)
+
+    done = False
     while not done:
         started = False
         # The get_candidates function supplies candidates, and is equivalent to the function "ConstructCandidates()" in the paper.
